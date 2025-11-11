188610da80c327788a31762e3c21b17e122f360d
Sheldon Hart <sheldon.hart88@gmail.com>
2025-11-11 14:34:06 +0200

    feat: Enhance logging initialization with JSON formatting and additional options

--- src/logging.rs
+++ src/logging.rs
@@ -1,11 +1,17 @@
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 pub fn init_logging() {
     if std::env::var("RUST_LOG").is_err() {
         panic!("RUST_LOG environment variable must be set");
     }
     tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer().with_ansi(false))
+        .with(
+            fmt::layer()
+                .json()
+                .with_ansi(false)
+                .with_current_span(true)
+                .with_span_list(false),
+        )
         .with(tracing_subscriber::EnvFilter::from_default_env())
         .with(tracing_error::ErrorLayer::default())
         .init();
